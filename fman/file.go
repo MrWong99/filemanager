68d2eb8ece587296492caded85bd82247ea42b11
Luk <Luk.Schm@web.de>
2021-11-18 12:21:03 +0100

    Added documentation and new read test

--- fman/file.go
+++ fman/file.go
@@ -8,37 +8,45 @@ import (
 	"time"
 )
 
+// A simple file datatype
 type File struct {
 	Path       string
 	LastUpdate time.Time
 }
 
+// Read the given file and return a slice of the contents split by line
 func (file *File) Read() ([]string, error) {
+	// Open the file and exit on any error
 	f, err := os.Open(file.Path)
 	if err != nil {
 		return nil, err
 	}
-	defer f.Close()
+	defer f.Close() // Defer means execute this when this function returns at any point
+
+	// Get the file info like last modified time or size
 	info, err := f.Stat()
 	if err != nil {
 		return nil, err
 	}
 	file.LastUpdate = info.ModTime()
 
+	// Scan the results line by line and append it to the result
 	scanner := bufio.NewScanner(f)
 	var result []string
 
 	for scanner.Scan() {
-		result = append(result[:], scanner.Text())
+		result = append(result, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
-	return result[:], nil
+	return result, nil
 }
 
+// Write the given content line by line into the file
 func (file *File) Write(content []string) error {
+	// Delete any file that might exist in the path
 	if exists(file.Path) {
 		fmt.Printf("File '%q' already exists, trying to delete...\n", file.Path)
 		err := os.Remove(file.Path)
@@ -46,12 +54,17 @@ func (file *File) Write(content []string) error {
 			return err
 		}
 	}
+
+	// Create a new file
 	f, err := os.Create(file.Path)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	file.LastUpdate = time.Now()
+
+	file.LastUpdate = time.Now() // File has just been created. Update last update time accordingly
+
+	// Write to the file
 	writer := bufio.NewWriter(f)
 	for _, line := range content {
 		_, err := writer.WriteString(line + "\n")
@@ -60,12 +73,15 @@ func (file *File) Write(content []string) error {
 		}
 	}
 	err = writer.Flush()
+
+	// Update last update time when no error occured
 	if err == nil {
 		file.LastUpdate = time.Now()
 	}
 	return err
 }
 
+// Check if given path exists
 func exists(path string) bool {
 	_, err := os.Stat(path)
 	return !errors.Is(err, os.ErrNotExist)
--- fman/file_test.go
+++ fman/file_test.go
@@ -7,51 +7,92 @@ import (
 	"time"
 )
 
+// Test the File.Read() method for the following myFunctionalities:
+// - initial LastUpdate time is a not initialized time.Time struct
+// - the read testdata has the expected content
+// - the LastUpdate timestamp is updated correctly after the Read()
 func TestRead(t *testing.T) {
 	file := File{
 		Path: "../testdata/for_test.txt",
 	}
+
+	// First test
 	var initialTime time.Time
 	if !initialTime.Equal(file.LastUpdate) {
 		t.Fatalf(`File.LastUpdate = %q, but expected is %q`, file.LastUpdate, initialTime)
 	}
+
+	// Second test
 	expected := []string{"Hello, cruel", "World!"}
 	msg, err := file.Read()
 	if !reflect.DeepEqual(expected, msg) || err != nil {
 		t.Fatalf(`File.Read() = %q, %v, but expected were %q, nil`, msg, err, expected)
 	}
+
+	// Third test
 	expectedTimestamp := time.Date(2021, 11, 17, 10, 00, 56, 171300800, time.Local)
 	if !expectedTimestamp.Equal(file.LastUpdate) {
 		t.Fatalf(`File.LastUpdate = %q, but expected is %q`, file.LastUpdate, expectedTimestamp)
 	}
 }
 
+// Test that a file not found error is returned upon wrong input
+func TestReadNotExists(t *testing.T) {
+	file := File{
+		Path: "../testdata/non_existend.file",
+	}
+	var initialTime time.Time
+	if !initialTime.Equal(file.LastUpdate) {
+		t.Fatalf(`File.LastUpdate = %q, but expected is %q`, file.LastUpdate, initialTime)
+	}
+	msg, err := file.Read()
+	if err == nil || !os.IsNotExist(err) {
+		t.Fatalf(`File.Read() = %q, %v, but expected were [], %v`, msg, err, os.ErrNotExist)
+	}
+}
+
+// Test the File.Write() method for the following checks:
+// - the test file does not exist before running the test
+// - the file was created successfully after the Write() call
+// - the file has the expected byte size
 func TestWrite(t *testing.T) {
 	testPath := "../testdata/temp_to_write.txt"
 	testContent := []string{"Hello", "Mehrnie :)"}
 	expectedTestSize := int64(len("Hello\nMehrnie :)\n"))
-	t.Cleanup(deleteFileFunction(testPath))
+
+	t.Cleanup(deleteFileFunction(testPath)) // Cleanup any created test files after the test finishes
+
 	file := File{
 		Path: testPath,
 	}
+
+	// First test
 	if exists(testPath) {
 		t.Fatalf(`Wrong test environment! File "%q" already exists`, testPath)
 	}
+
 	file.Write(testContent)
 	resFile, err := os.Stat(testPath)
+
+	// Second test
 	if err != nil {
 		t.Fatalf(`Test file %q was not created by Write(). Error: %v`, testPath, err)
 	}
+
+	// Third test
 	if resFile.Size() != expectedTestSize {
 		t.Fatalf(`Test file %q had size %d, but expected is %d`, testPath, resFile.Size(), expectedTestSize)
 	}
 }
 
+// This method returns a myFunction that can be called to delete the file under given path if it exists
 func deleteFileFunction(path string) func() {
-	fnc := func() {
+	myFunction := func() {
 		if exists(path) {
 			os.Remove(path)
 		}
 	}
-	return fnc
+	// with this myFunction in a variable you could write this to call it:
+	// myFunction()
+	return myFunction
 }
